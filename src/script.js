e92421eeca1db4b4821b7f9fceede6aa9768c88f
Kealan Parr <kealantparr@gmail.com>
2022-09-30 12:26:28 +0100

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,51 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+let time = Date.now()
+
+const tick = () => {
+
+	const currentTime = Date.now()
+	const delta = currentTime - time
+	time = currentTime
+
+	console.log(delta)
+
+	mesh.rotation.y += 0.001 * delta
+	mesh.rotation.x += 0.001 * delta
+
+	renderer.render(scene, camera)
+
+	window.requestAnimationFrame(tick)
+}
+
+tick()
